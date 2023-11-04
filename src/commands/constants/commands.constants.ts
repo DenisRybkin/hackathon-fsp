2b5ad07f02fd0c6a3a7b7829ebb74806822c9320
Andrey Shulmin <ashulmin14@gmail.com>
2023-11-04 03:50:24 +0300

    small fix

--- src/commands/constants/commands.constants.ts
+++ src/commands/constants/commands.constants.ts
@@ -1,10 +1,6 @@
 export enum CommandConstants {
   GetStats = 'get_stats',
   AddConnection = 'add_connection',
-<<<<<<< HEAD
-=======
-  Init = 'init',
   GetDashboard = 'get_dashboard',
->>>>>>> f99c66beb6c86811ef252eaf795096f5abffc2ae
 }
 
