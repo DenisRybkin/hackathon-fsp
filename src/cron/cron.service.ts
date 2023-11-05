2da3af733096829e10162ba72c7671aa74f89a5d
s.talabael <s.talabaev@setupit.org>
2023-11-05 04:59:54 +0300

    add send status +80% data in datebase

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -10,8 +10,9 @@ import {
   SendLongTransactionMessage,
   TerminateHandler,
 } from './utils/long-transactions.utils'
-import { GetMemoryDatabase } from './utils/get-memory.utils'
+import { GetMemoryDatabase, SendFullDatebaseMessage } from './utils/memory.utils'
 import { Memory } from '../modules/account/domain/entities/memory.entity'
+import { log } from 'console'
 
 const checkLongTransaction = async (
   bot: Telegraf<IBotContext>,
@@ -48,27 +49,56 @@ const checkMemoryDatabase = async (
   console.log("test")
   const memory = await GetMemoryDatabase(connection); 
   const currentLastStateMemory = connection?.Memories?.[(connection.Memories?.length ?? 0) - 1 ]
-  if (!currentLastStateMemory) {
+  
+  if (!currentLastStateMemory?.Usage) {
     connection.addMemory(new Memory(memory[0].pg_database_size))
-  } 
-  else if (currentLastStateMemory && currentLastStateMemory !== memory[0].pg_database_size) {
-    connection.addMemory(new Memory(memory[0].pg_database_size))
-  } 
+  }
+  if(currentLastStateMemory?.Usage) {
+    if (+currentLastStateMemory.Usage !== +memory[0].pg_database_size) {
+    
+      connection.addMemory(new Memory(memory[0].pg_database_size))
+    } 
+  }
+  
   try{
     await connectionRepo.save(connection)
   } catch (e) {
     console.log(e)
   }
-  console.log("Model::::::::", connection.Memories)
+  //console.log("Model::::::::", connection.Memories)
+  console.log("size::::::::", memory[0].pg_database_size)
+}
+
+const checkFullMemoryDatebaseConnection = async (
+  bot: Telegraf<IBotContext>,
+  connection: Connection
+) => {
+    const memory = await GetMemoryDatabase(connection);
+    const currentLastStateMemory = connection?.Memories?.[0]
+    let firstStateMemory = connection?.Memories?.[(connection.Memories?.length ?? 0) - 1 ]
+    console.log("firstStateMemory", firstStateMemory, '---', currentLastStateMemory, "currentLastStateMemory")
+    if (firstStateMemory && currentLastStateMemory) {
+
+      const difference = ((+currentLastStateMemory.Usage / +firstStateMemory.Usage) * 100) - 100
+      console.log("firstStateMemory", firstStateMemory, "currentLastStateMemory", currentLastStateMemory)
+      console.log("difference", difference)
+      if (difference > 80) {
+      
+      if (difference > 10) {
+        await SendFullDatebaseMessage(bot, connection)
+    }
+    }
+  } 
 }
 
 export class CronService implements ICronService {
   constructor(private readonly bot: Telegraf<IBotContext>) {}
 
   async init() {
-    await this.asyncAnalyzeLongTransaction();
-    await this.asyncAnalyzeDeadlocks();
+    //await this.asyncAnalyzeLongTransaction();
+    //await this.asyncAnalyzeDeadlocks();
     await this.analizeUseMemory();
+    //await this.checkFullMemoryDatebase();
   }
 
   async asyncAnalyzeLongTransaction() {
@@ -115,6 +145,21 @@ export class CronService implements ICronService {
       } catch (e) {
         console.log('error:', e);
       }
+    })
+  }
+  async checkFullMemoryDatebase() {
+    cron.schedule('*/5 * * * * *', async() => {
+      const connectionRepo = new ConnectionRepositoryImpl();
+      const connections = await connectionRepo.find(true);
+      if(!connections) return null;
+      try {
+        for (const connection of connections) {
+          await checkFullMemoryDatebaseConnection(this.bot, connection)
+        }
+      } catch (e) {
+        console.log('error:', e);
+      }
+
     })
   }
 }
--- src/cron/utils/memory.utils.ts
+++ src/cron/utils/memory.utils.ts
@@ -1,5 +1,7 @@
 import { Connection } from '../../modules/account/domain/entities/connection.entity';
 import { DbClientService } from '../../database/db-client.service';
+import { Telegraf } from 'telegraf'
+import { IBotContext } from '../../context/context.interface'
 
 export const GetMemoryDatabase = async (connection: Connection) => {
   const cl = new DbClientService({
@@ -12,6 +14,16 @@ export const GetMemoryDatabase = async (connection: Connection) => {
 
   const transactions = await cl.getMemory(connection.Database);
   return transactions;
+};
+
+
+export const SendFullDatebaseMessage = (
+  bot: Telegraf<IBotContext>,
+  connection: Connection,
+) => {
+  const message = `the total disk space occupied by the database has been increased by 80%+`;
+
+  return bot.telegram.sendMessage(Number(connection.Account?.Id), message);
 };
 
 
